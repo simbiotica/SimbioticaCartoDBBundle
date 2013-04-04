f628ea94aba2ddb1ad489fa568efee32d2c971e2
tiagojsag <tiagojsag@gmail.com>
2013-04-04 13:49:37 +0200

    Fix some details in SymfonySessionStorage

--- CartoDB/SymfonySessionStorage.php
+++ CartoDB/SymfonySessionStorage.php
@@ -2,9 +2,9 @@
 
 namespace Simbiotica\CartoDBBundle\CartoDB;
 
+use Simbiotica\CartoDBClient\TokenStorageInterface;
 use Eher\OAuth\Token;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Simbiotica\CartoDBClient\TokenStorageInterface;
 
 class SymfonySessionStorage implements TokenStorageInterface
 {
@@ -15,15 +15,15 @@ class SymfonySessionStorage implements TokenStorageInterface
     
     function __construct(Session $session, $subdomain)
     {
-        $this->sessionKey = Connection::SESSION_KEY_SEED.'-'.$subdomain;
+        $this->sessionKey = SymfonySessionStorage::SESSION_KEY_SEED.'-'.$subdomain;
         $this->session = $session;
     }
     
-    protected function getToken() {
+    public function getToken() {
         return unserialize($this->session->get($this->sessionKey, null));
     }
     
-    protected function setToken(Token $token) {
+    public function setToken(Token $token) {
         if ($this->session == null)
             throw new \RuntimeException("Need a valid session to store CartoDB auth token");
         $this->session->set($this->sessionKey, serialize($token));
